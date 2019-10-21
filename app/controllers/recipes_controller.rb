3b874a6b437fcbd3752937c45e8f9c5fc1901628
Reina <r.clarkemitchell@gmail.com>
2019-10-20 22:36:20 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "First Ingredient")
+    @recipe.ingredients.build(name: "Second Ingredient")
   end
 
   def create
+    # byebug
+    @recipe = Recipe.create(params_ing)
+    redirect_to @recipe
   end
+
+private
+def params_ing
+  params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+
+end
+
+
 end
